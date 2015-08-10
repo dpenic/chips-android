04d3e0a9721cbcafdb6b0ccb16cc831e48c4982e
Dario Penic <dario.penic@gmail.com>
2015-08-10 12:22:18 +0200

    trying

--- library/src/main/java/com/eyeem/chips/ChipsEditText.java
+++ library/src/main/java/com/eyeem/chips/ChipsEditText.java
@@ -68,7 +68,7 @@ public class ChipsEditText extends MultilineEditText {
       setCursorVisible(false);
 
       // default bubble & cursor style
-      setCurrentBubbleStyle(DefaultBubbles.get(DefaultBubbles.GRAY_WHITE_TEXT, getContext(), (int) getTextSize()));
+//      setCurrentBubbleStyle(DefaultBubbles.get(DefaultBubbles.GRAY_WHITE_TEXT, getContext(), (int) getTextSize()));
       this.savedHint = getHint();
    }
 
